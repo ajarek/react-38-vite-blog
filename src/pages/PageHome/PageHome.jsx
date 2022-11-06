a66d3fe9d1ea419799c5f079b7bf7013b04fcb49
Ajarek <ajarek@poczta.onet.pl>
2022-11-06 21:47:11 +0100

    added cards

--- src/pages/PageHome/PageHome.jsx
+++ src/pages/PageHome/PageHome.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import data from '../.././assets/db.json'
+import './PageHome.css'
 export const PageHome = () => {
-  return <div>
-    PageHome
+  // console.log(data.authors)
+  return (
+    <div 
+    className={'home'}
+    >
+      {data.authors.map((author) => {
+        return (
+          <div
+            className={'card'}
+            key={author.id}
+          >
+            <img
+              src={`https://joeschmoe.io/api/v1/${author.firstName}`}
+              alt=''
+            />
+            <p>{author.firstName + ' ' + author.lastName}</p>
+            <button>Click to view Profile</button>
+          </div>
+        )
+      })}
     </div>
+  )
 }
 
 export default PageHome
