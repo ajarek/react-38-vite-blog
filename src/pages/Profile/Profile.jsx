b9f99225a0521bf2e598a947aaf12de1c0b6c2d0
Ajarek <ajarek@poczta.onet.pl>
2022-11-08 22:13:32 +0100

    filter posts added

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import data from '../.././assets/db.json'
 import { NavLink } from 'react-router-dom'
@@ -8,7 +8,22 @@ export const Profile = () => {
   const { id } = useParams()
   const found = data.authors.find((el) => el.id === id)
   const postsId = data.posts.filter((el) => el.authorId === Number(id))
+ 
+  const [parameters,setParameters] = useState('id')
 
+  const sortPosts=()=> {
+  if(parameters==='id')
+  {return  postsId.sort((a,b)=>b.id-a.id)}
+  if(parameters==='dateAscending')
+  {return  postsId.sort((a,b)=>b.datePublished-a.datePublished)}
+  if(parameters==='dateDescending')
+  {return  postsId.sort((a,b)=>a.datePublished-b.datePublished)}
+  if(parameters==='likesAscending')
+  {return  postsId.sort((a,b)=>b.numLikes-a.numLikes)}
+  if(parameters==='likesDescending')
+  {return  postsId.sort((a,b)=>a.numLikes-b.numLikes)}
+  
+  }
   return (
     <div className={'profile'}>
       <div className={'profile-card'}>
@@ -23,7 +38,11 @@ export const Profile = () => {
       </div>
       <div className='posts-id'>
         <h2>Posts</h2>
-        {postsId.map((el) => {
+        <button onClick={()=>setParameters('dateAscending')}>Date Ascending</button>
+        <button onClick={()=>setParameters('dateDescending')}>Date Descending</button>
+        <button onClick={()=>setParameters('likesAscending')}>Likes Ascending</button>
+        <button onClick={()=>setParameters('likesDescending')}>Likes Descending</button>
+        {sortPosts().map((el) => {
           return (
             <NavLink
               className={'button-link'}
